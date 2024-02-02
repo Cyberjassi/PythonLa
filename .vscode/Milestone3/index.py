e3f1f687b515f9edd1454096eeffd8071a3e0d61
jaswant khatri <jaswantkhatri30@gmail.com>
2024-02-02 18:54:26 +0530

    2feb files added

--- .vscode/Milestone3/index.py
+++ .vscode/Milestone3/index.py
@@ -1,27 +1,43 @@
-menu = input("What you want to Do? Add List Find for that - respectively (a,l,f)\n And for Quit press q\n")
 
 movies = []
+while True:
+    menu = input("What do you want to do? Add, List, Find, or Quit - respectively (a, l, f, q)\n")
 
-while menu!='q':
     if menu == 'a':
-        title = input("Your movie title name-\n")
-        name = input("Your movie  name-\n")
-        director = input("Your movie director name-\n")
-        
+        print("You chose to Add a Movie:\n")
+        title = input("Your movie title name:\n")
+        name = input("Your movie name:\n")
+        director = input("Your movie director name:\n")
+        print("\nYour Movie is Added:\n")
         newdic = {
-            "title":title,
-            "name":name,
-            "director":director
+            "title": title,
+            "name": name,
+            "director": director
         }
         movies.append(newdic)
-        break
 
     elif menu == 'l':
-        pass
+        print("\nHere are all your movies:")
+        print(f"\nYour all movies -\n{movies}")
+
     elif menu == 'f':
-        pass
+        print("You chose to Find Your Movie:\n")
+        findbytitle = input("Give me your Movie title name:\n")
+        found = False
+        for i in movies:
+            if findbytitle == i['title']:
+                print(i)
+                found = True
+                break
+
+        if not found:
+            print("Sorry, your movie is not in your movie list.\n")
+
     elif menu == 'q':
         exit()
-    else:
-        print("You Typed Wrong Operation! ")
 
+    else:
+        menu = input("\nWhat do you want to do? Add, List, Find - respectively (a, l, f)\n Or Quit by pressing q\n")
+       
+
+   
