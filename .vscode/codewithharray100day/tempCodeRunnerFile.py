74946678090dc340271c1eaddebe6ef0f434c880
jaswant khatri <jaswantkhatri30@gmail.com>
2024-02-06 18:54:30 +0530

    6feb

--- .vscode/codewithharray100day/tempCodeRunnerFile.py
+++ .vscode/codewithharray100day/tempCodeRunnerFile.py
@@ -1,2 +1,9 @@
-
-else:
+if(code):
+    ans = []
+    for i in range(1,len(arr)):
+        if(len(i)>3):
+            nwi = i[1:]+i[0]
+            ans.append(nwi)
+        else:
+            ran = 'dfg'
+            ans.append(ran+i[::-1]+ran)
